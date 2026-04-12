4a2951d6add112299de3fefd411b370a82cb7c3a
johannajonsson <jojo.risin@outlook.com>
2026-04-12 16:46:37 +0200

    second commit. Update architecture: remove order and keep user and authentication. create workflow.yml and dockerfile

--- src/main/java/se/jensen/johanna/fakestoreapi/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package se.jensen.johanna.fakestoreapi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import se.jensen.johanna.fakestoreapi.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-  
-
-}
